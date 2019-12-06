d7c31043797665992da00e4c867ede5f90626cd1
John Pavlik <john.a.pavlik@gmail.com>
2019-12-05 19:07:05 -0600

    Update MultiImageGaussianMixtureModel.py

--- MultiImageGaussianMixtureModel.py
+++ MultiImageGaussianMixtureModel.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import scipy.stats
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.colors
 
 #Gaussian Mixture Model with tied covariance matrices
 rows = []
@@ -55,7 +56,7 @@ for iteration in range(num_iterations):
         old_posterior_probs = posterior_probs
     posterior_probs = []
     for cluster in range(num_clusters):
-        posterior_probs.append(np.nan_to_num(tmp_probs[cluster] / all_normals))
+        posterior_probs.append(np.nan_to_num(tmp_probs[cluster] / all_normals, nan=1e-10, posinf=1e10))
 
 
     if old_posterior_probs:
@@ -63,6 +64,9 @@ for iteration in range(num_iterations):
         print("Delta = ", delta)
         if delta < 0.01:
             break
+
+    if iteration == num_iterations - 1:
+        break
 
     # M-step: Recalculate the mean/covariance for each class given the
     # current pixel probabilities for that class
@@ -96,16 +100,35 @@ for iteration in range(num_iterations):
 #         plt.savefig('Output/' + filenames[image].replace('.jpg', f"_{cluster}.jpg"), dpi=200)
 #     start += rows[image] * cols[image]
 
+#Colormap builder code from https://stackoverflow.com/questions/55501860/how-to-put-multiple-colormap-patches-in-a-matplotlib-legend
+from matplotlib.legend_handler import HandlerBase
+class HandlerColormap(HandlerBase):
+    def __init__(self, cmap, num_stripes=8, **kw):
+        HandlerBase.__init__(self, **kw)
+        self.cmap = cmap
+        self.num_stripes = num_stripes
+    def create_artists(self, legend, orig_handle,
+                       xdescent, ydescent, width, height, fontsize, trans):
+        stripes = []
+        for i in range(self.num_stripes):
+            s = mpatches.Rectangle([xdescent + i * width / self.num_stripes, ydescent],
+                          width / self.num_stripes,
+                          height,
+                          fc=self.cmap((2 * i + 1) / (2 * self.num_stripes)),
+                          transform=trans)
+            stripes.append(s)
+        return stripes
+
 #Combine probs into a single image with different colors (assumes 5 clusters so has 5 colors)
-colors = np.array([[0, 135, 68], [214, 45, 32], [255, 167, 0], [153, 51, 255], [0, 87, 231]]) / 255
+my_colors = np.array([[0, 135, 68], [214, 45, 32], [255, 167, 0], [153, 51, 255], [0, 87, 231]]) / 255
 start = 0
 for image in range(len(filenames)):
     end = start + rows[image] * cols[image]
     image_data = np.empty((num_clusters, rows[image] * cols[image], 3))
     for cluster in range(num_clusters):
-        tmp_r = np.multiply(colors[cluster][0], posterior_probs[cluster][start : end])
-        tmp_g = np.multiply(colors[cluster][1], posterior_probs[cluster][start : end])
-        tmp_b = np.multiply(colors[cluster][2], posterior_probs[cluster][start : end])
+        tmp_r = np.multiply(my_colors[cluster][0], posterior_probs[cluster][start: end])
+        tmp_g = np.multiply(my_colors[cluster][1], posterior_probs[cluster][start: end])
+        tmp_b = np.multiply(my_colors[cluster][2], posterior_probs[cluster][start: end])
         image_data[cluster] = np.dstack((tmp_r, tmp_g, tmp_b))
     combined_data = np.mean(image_data, axis=2)
     max_cluster = np.argmax(combined_data, axis=0)
@@ -114,15 +137,19 @@ for image in range(len(filenames)):
         for y in range(cols[image]):
             for color in range(3):
                 pixels[x, y] = image_data[max_cluster[x*cols[image] + y], x*cols[image] + y]
-    tmp_max = np.max(pixels.flatten())
-    plt.imshow(pixels / tmp_max)
+    #tmp_max = np.max(pixels.flatten())
+    plt.imshow(pixels)# / tmp_max)
+    cmaps = [matplotlib.colors.LinearSegmentedColormap.from_list("", [my_colors[0] * 0.1, my_colors[0]]),
+             matplotlib.colors.LinearSegmentedColormap.from_list("", [my_colors[1] * 0.1, my_colors[1]]),
+             matplotlib.colors.LinearSegmentedColormap.from_list("", [my_colors[2] * 0.1, my_colors[2]]),
+             matplotlib.colors.LinearSegmentedColormap.from_list("", [my_colors[3] * 0.1, my_colors[3]]),
+             matplotlib.colors.LinearSegmentedColormap.from_list("", [my_colors[4] * 0.1, my_colors[4]])]
+    cmap_labels = ["Class 1", "Class 2", "Class 3", "Class 4", "Class 5"]
+    cmap_handles = [mpatches.Rectangle((0, 0), 1, 1) for _ in cmaps]
+    handler_map = dict(zip(cmap_handles,
+                           [HandlerColormap(cm, num_stripes=128) for cm in cmaps]))
+    plt.legend(handles=cmap_handles, labels=cmap_labels, handler_map=handler_map)
     #plt.show()
-    red = mpatches.Patch(color=tuple(colors[0]), label='Class 1')
-    green = mpatches.Patch(color=tuple(colors[1]), label='Class 2')
-    blue = mpatches.Patch(color=tuple(colors[2]), label='Class 3')
-    purple = mpatches.Patch(color=tuple(colors[3]), label='Class 4')
-    yellow = mpatches.Patch(color=tuple(colors[4]), label='Class 5')
-    plt.legend(handles=[red, green, blue, purple, yellow])
     plt.savefig('Output/' + filenames[image].replace('.jpg', f"_combined.jpg"), dpi=600)
     start += rows[image] * cols[image]
 
